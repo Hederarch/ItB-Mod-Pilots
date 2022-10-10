6b513ba7681db411b66f6d92ce1aaf35d7ad7c11
Hederarch <66890769+Hederarch@users.noreply.github.com>
2022-10-10 01:58:11 -0500

    Modpacked
    No actual changes I hope, just moved to one folder

---
+++ Hex Pilots/scripts/init.lua
@@ -0,0 +1,9 @@
+return {
+	id = "hedera_pilots",
+	name = "Hex Pilots",
+	description = "6 unique pilots that add new challenges to runs.",
+	icon = "scripts/icon.png",
+	submodFolders = {"mods/"},
+	init = function() end,
+	load = function() end
+}
